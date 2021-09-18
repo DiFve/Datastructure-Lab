4caa893531afbeefe9f7d13e37b62a3f1a8bc960
DiFve <jkblade6@gmail.com>
2021-09-19 00:52:27 +0700

    Lab06

---
+++ Lab06/63010789_Lab06_5.py
@@ -0,0 +1,21 @@
+def pyramid(x,i):
+    x = int(x)
+    f="_"
+    b="#"
+    if x>0 and i<x+1:
+        f*=(x-i)
+        b*=i
+        print(f+b)
+        pyramid(x,i+1)
+    elif x<0 and i<(-x+1):
+        x = -x
+        f*=i-1
+        b*=(x-i)+1
+        print(f+b)
+        pyramid(-x,i+1)
+    elif x==0:
+        print("Not Draw!")
+        
+
+inp = input("Enter Input : ")
+pyramid(inp,1)
